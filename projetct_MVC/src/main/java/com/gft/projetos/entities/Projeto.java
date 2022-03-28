cb0de60760a22e485067c66304f660514cf19ad2
Renan Almeida <98130954+Azkalum@users.noreply.github.com>
2022-03-28 15:25:57 -0300

    Add files via upload

---
+++ projetct_MVC/src/main/java/com/gft/projetos/entities/Projeto.java
@@ -0,0 +1,84 @@
+package com.gft.projetos.entities;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+@Entity
+public class Projeto {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	private String apelido;
+	
+	@ManyToOne
+	private Linguagem linguagem;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date dataEntrega;
+	
+	private BigDecimal orcamentoLong;
+	
+	@ManyToMany
+	private List<Desenvolvedor> desenvolvedores;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getApelido() {
+		return apelido;
+	}
+	public void setApelido(String apelido) {
+		this.apelido = apelido;
+	}
+	public Linguagem getLinguagem() {
+		return linguagem;
+	}
+	public void setLinguagem(Linguagem linguagem) {
+		this.linguagem = linguagem;
+	}
+	public BigDecimal getOrcamentoLong() {
+		return orcamentoLong;
+	}
+	public void setOrcamentoLong(BigDecimal orcamentoLong) {
+		this.orcamentoLong = orcamentoLong;
+	}
+	public Date getDataEntrega() {
+		return dataEntrega;
+	}
+	public void setDataEntrega(Date dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+	public List<Desenvolvedor> getDesenvolvedores() {
+		return desenvolvedores;
+	}
+	public void setDesenvolvedores(List<Desenvolvedor> desenvolvedores) {
+		this.desenvolvedores = desenvolvedores;
+	}
+	
+	
+
+}
---
+++ treinamentoMvc/src/main/java/com/gft/treinamento/controllers/PersonController.java
@@ -0,0 +1,104 @@
+package com.gft.treinamento.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.gft.treinamento.entities.Person;
+import com.gft.treinamento.services.PersonService;
+
+@Controller
+@RequestMapping ("/person")
+
+public class PersonController {
+	
+	@Autowired
+	PersonService PersonService;
+
+	@RequestMapping(method = RequestMethod.GET, path="/get")
+    public ModelAndView getPerson(@RequestParam Long id) {
+    	
+    	ModelAndView mv = new ModelAndView("person.html");
+    	
+    	try {
+    	Person person = PersonService.getPerson(id);
+    	
+    	mv.addObject("person", person);
+    	
+    	}catch(Exception e) {
+    		
+    		mv.addObject("message", e.getMessage());
+    	}
+    	
+    	return mv;
+    	
+    }
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/new")
+	public ModelAndView formPerson() {
+		
+		ModelAndView mv = new ModelAndView("formperson.html");
+		mv.addObject("person", new Person());
+		
+		return mv;
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path="/edit")
+	public ModelAndView editPerson(@RequestParam Long id) {
+		
+		ModelAndView mv = new ModelAndView("formperson.html");
+		
+		try {
+			Person person = PersonService.getPerson(id);
+			mv.addObject("person", person);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return mv;
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView newPerson(Person person, RedirectAttributes redirectAttributes) {
+		
+		ModelAndView mv = new ModelAndView("redirect:/person");
+		
+		PersonService.savePerson(person);
+		
+		redirectAttributes.addFlashAttribute("message", "Pessoa salva com sucesso");
+		
+		return mv;
+		
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, path="/delete")
+	public ModelAndView deletePerson(@RequestParam Long id, RedirectAttributes redirectAttributes) {
+		
+		ModelAndView mv = new ModelAndView("redirect:/person");
+		try {
+		    PersonService.deletePerson(id);	
+		    redirectAttributes.addFlashAttribute("message", "Pessoa excluída com sucesso");
+		}catch(Exception e) {
+			redirectAttributes.addFlashAttribute("message", e.getMessage());
+		}
+				
+		return mv;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView listPerson() {
+		
+		ModelAndView mv = new ModelAndView("listperson.html");
+		mv.addObject("list", PersonService.listPerson());
+		
+		return mv;
+		
+	}
+}
